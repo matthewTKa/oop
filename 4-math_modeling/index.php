7a47de3cd88998d8e4adea68fc72edcb2bca3c68
MatthewTKa <mat.17.01@icloud.com>
2021-03-02 19:16:29 +0700

    new-arr

--- 4-math_modeling/index.php
+++ 4-math_modeling/index.php
@@ -3,9 +3,11 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/4-math_modeling/Game.php';
 
 $arr = [
-    [3,2,5,10],
-    [1,5,100,9],
-    [13,32,33,9]
+    [2,3,5,9],
+    [-2,-4,-2,7],
+    [7,5,0,-3],
+    [-1,6,1,2],
+    [6,9,6,3]
 ];
 
 $game = new Game($arr,1);
